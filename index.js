2ce41e911eaf6b4bb994f7d3b2ef8cf47b4c03a4
Yan Hong <onlyhongyan@gmail.com>
2016-06-03 00:14:58 -0700

    add search feautre

--- index.js
+++ index.js
@@ -1,44 +1,75 @@
 const express = require('express');
 const request = require("request");
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-var app = express();
-var db;
+const ObjectId = require('mongodb').ObjectID;
 
-if (!Date.now) {
-    Date.now = function() { return new Date().getTime(); }
+var app = express();
+var db, siteCollection;
+
+app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({extended: true}))
+
+Date.now = function() {
+    var currentTime = new Date()
+    var datevalues = currentTime.getFullYear() + '/' + (currentTime.getMonth()+1) + '/' + currentTime.getDate() + ' ' + currentTime.getHours() + ':' + currentTime.getMinutes() + ':' +
+        currentTime.getSeconds()
+    return datevalues
 }
 
 MongoClient.connect('mongodb://localhost:27017/scrappy', (err, database) => {
     if (err) return console.log(err)
     db = database
+    siteCollection = db.collection('sites')
+    db.ensureIndex("sites", {
+        content: "text"
+    }, function(err, indexname) {
+        // assert.equal(null, err);
+    })
     app.listen(3000, () => {
         console.log('listening on 3000')
     })
 })
 
 app.get('/', (req, res) => {
-    var collection = db.collection('sites')
-    var results = collection.find().toArray(function(err, docs) {
-        res.send(docs)
-        db.close()
+    siteCollection.find().toArray(function(err, docs) {
+        if (err) return console.log(err)
+        res.render('index.pug', {documents: docs})
+    })
+})
+
+app.get('/document/:docId', (req, res) => {
+    siteCollection.findOne({_id: ObjectId(req.params.docId)}, function(err, docs) {
+        if (err) return console.log(err)
+        res.render('document.pug', {document: docs})
     })
 })
 
 app.post('/add', (req, res) => {
-    var requestURL = "http://google.com/"
+    var requestURL = req.body.url
     request({
         uri: requestURL,
     }, (error, response, body) => {
-    console.log(Date.now());
-    var collection = db.collection('sites')
-    collection.insert({
+    siteCollection.insert({
             url: requestURL,
-            timestamp: Date.now(),
+            created: Date.now(),
+            category: req.body.category,
             content: body
         }, function(err, result) {
             if (err) return console.log(err)
-            res.send(result)
-            db.close()
+            console.log('Content Saved to DB. ' + Date.now())
+            res.redirect('/')
         })
+    })
+})
+
+app.post("/search", (req, res) => {
+    siteCollection.find({
+        "$text": {
+            "$search": req.body.query
+        }
+    }).toArray(function(err, docs) {
+        if (err) return console.log(err)
+        res.render('index.pug', {documents: docs})
     })
 })
